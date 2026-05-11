be423f27f920f635a459b4eebfb219cb26de831e
Yuvraj-Singh-28 <yuvi5911y@gmail.com>
2026-05-11 14:55:24 +1200

    added some line of code and moved some

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from os import name
-
+import json
 from flask import Flask, render_template, request, redirect, url_for,session, flash
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 
-if name == 'main':
+def load_data():
+    with open('data/flowers.json') as file:
+     flowers = json.load(file)
+     return flowers
+    
+@app.route('/')
+def index():
+ return render_template('index.html')
+
+@app.route('/')
+def index():
+ flowers = load_data()
+ return render_template('index.html', flowers=flowers)
+
+
+# This is the last line of the code.
+
+if __name__ == '__main__':
     app.run(debug=True)
     
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+# last line ends.    
+    
